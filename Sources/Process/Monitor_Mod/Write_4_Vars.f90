6280e157fed663c2aaf6f498a337fbec5c1bc90d
mhadziabdic <muhamed.hadziabdic@gmail.com>
2019-04-15 08:44:45 +0200

    Total time and PM10 columns are added.

--- Sources/Process/Monitor_Mod/Write_4_Vars.f90
+++ Sources/Process/Monitor_Mod/Write_4_Vars.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Monitor_Mod_Write_4_Vars(n, flow)
+  subroutine Monitor_Mod_Write_4_Vars(n, time, flow)
 !------------------------------------------------------------------------------!
 !   This is to set up monitoring points.                                       !
 !------------------------------------------------------------------------------!
@@ -10,13 +10,20 @@
 !-----------------------------------[Locals]-----------------------------------!
   type(Var_Type),  pointer :: u, v, w, p
   integer                  :: m
+  real                     :: vel_mag
+  real                     :: time
 !==============================================================================!
 
   do m = 1, monitor % n_points
     if(monitor % cell(m) > 0) then
-      write(10+m,'(i9,5e16.6)')  n,  flow % u % n(monitor % cell(m)),  &
+      vel_mag = sqrt( (flow % u % n(monitor % cell(m)))**2 &
+                   +  (flow % v % n(monitor % cell(m)))**2 &
+                   +  (flow % w % n(monitor % cell(m)))**2 )
+      write(10+m,'(i9,8e16.6)')  n, time*1000.0/60.0,                  & 
+                                     flow % u % n(monitor % cell(m)),  &
                                      flow % v % n(monitor % cell(m)),  &
                                      flow % w % n(monitor % cell(m)),  &
+                                     vel_mag,                          &
                                      flow % p % n(monitor % cell(m)),  &
                           180.0*ATAN(flow % v % n(monitor % cell(m)) / &
                               flow % u % n(monitor % cell(m)))/3.14159
--- Sources/Process/Monitor_Mod/Write_5_Vars.f90
+++ Sources/Process/Monitor_Mod/Write_5_Vars.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Monitor_Mod_Write_5_Vars(n, flow)
+  subroutine Monitor_Mod_Write_5_Vars(n, time, flow)
 !------------------------------------------------------------------------------!
 !   This is to set up monitoring points.                                       !
 !------------------------------------------------------------------------------!
@@ -9,18 +9,31 @@
   integer          :: n
 !-----------------------------------[Locals]-----------------------------------!
   type(Var_Type),  pointer :: u, v, w, p, t
+  type(Grid_Type), pointer :: grid
   integer                  :: m
+  real                     :: vel_mag
+  real                     :: time
 !==============================================================================!
+
+  ! Take aliases
+  grid => flow % pnt_grid
 
   do m = 1, monitor % n_points
     if(monitor % cell(m) > 0) then
-      write(10+m,'(i9,6e16.6)')  n,  flow % u % n(monitor % cell(m)),  &
+      vel_mag = sqrt(( flow % u % n(monitor % cell(m)) )**2 &
+                   + ( flow % v % n(monitor % cell(m)) )**2 &
+                   + ( flow % w % n(monitor % cell(m)) )**2 )
+      write(10+m,'(i9,9e16.6)')  n, time*1000.0/60.0,                  &
+                                     flow % u % n(monitor % cell(m)),  &
                                      flow % v % n(monitor % cell(m)),  &
                                      flow % w % n(monitor % cell(m)),  &
+                                     vel_mag,                          &
                                      flow % p % n(monitor % cell(m)),  &
-                          180.0*ATAN(flow % v % n(monitor % cell(m)) / &
-                             flow % u % n(monitor % cell(m)))/3.14159, &
-                                     flow % t % n(monitor % cell(m))
+                          180.0*ATAN(flow % v % n(monitor % cell(m)) / & 
+                              flow % u % n(monitor % cell(m)))/3.14159,&
+                                     flow % t % n(monitor % cell(m))   
+                                     
+
     end if
   end do
 
