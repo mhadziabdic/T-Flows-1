1b1bff43789dbffe4f087beeaa89c398a4cdf330
mhadziabdic <muhamed.hadziabdic@gmail.com>
2019-05-01 16:06:32 +0200

    River_angle is added.

--- Sources/Process/Turbulence/Allocate.f90
+++ Sources/Process/Turbulence/Allocate.f90
@@ -125,6 +125,8 @@
     if(rough_walls) then
       allocate(z_o_f(-grid % n_bnd_cells:grid % n_cells));  z_o_f   = -1.0
       allocate(c_o_f(-grid % n_bnd_cells:grid % n_cells));  c_o_f   = 0.
+      allocate(river_angle(-grid % n_bnd_cells:grid % n_cells));  &
+                                                      river_angle   = 0.
       allocate(id_zone(-grid % n_bnd_cells:grid % n_cells));id_zone = 0
     end if
 
