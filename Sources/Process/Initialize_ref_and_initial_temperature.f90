ffb352897bdec62033e3542334e69e3bd6175954
mhadziabdic <muhamed.hadziabdic@gmail.com>
2019-05-31 15:35:49 +0200

    Minor changes.

--- Sources/Process/Initialize_ref_and_initial_temperature.f90
+++ Sources/Process/Initialize_ref_and_initial_temperature.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Initialize_ref_and_initial_temperature(flow, backup)
+  subroutine Initialize_ref_and_initial_temperature(flow, backup, time_step)
 !------------------------------------------------------------------------------!
 !   Initialize dependent variables.  (It is a bit of a mess still)             !
 !------------------------------------------------------------------------------!
@@ -25,8 +25,9 @@
   type(Var_Type),  pointer :: u, v, w, t
   integer                  :: i, c, c1, c2, m, s, nks, nvs
 
-  integer                  :: n_points, k
+  integer                  :: n_points, k, n_wall, time_step
   real, allocatable        :: prof(:,:), x(:), y(:), z(:), dist(:)
+  real                     :: t_wall
   logical                  :: backup, present
 
 !==============================================================================!
@@ -35,31 +36,44 @@
   grid => flow % pnt_grid
   t    => flow % t
 
+  t_wall = 0.0
+  n_wall = 0
   
-  if (this_proc < 2) print *, '# Grid material: ', grid % material % name
-
   if(.not. backup) then
     do c = 1, grid % n_cells
 
       if(heat_transfer) then
         t % n(c)  = 5.0 + 4.0 * grid % wall_dist(c)
-!        t % n(c)  = 5.0 + 4.0 * grid % wall_dist(c)
-!        t % n(c)  = 21.9 + 21.5 * grid % wall_dist(c)
         t % o(c)  = t % n(c)
         t % oo(c) = t % n(c)
       end if
     end do ! through cells
   end if
 
+  do s = 1, grid % n_faces
+    c1 = grid % faces_c(1,s)
+    c2 = grid % faces_c(2,s)
+
+    if(c2  < 0) then
+      if( Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. wallfl ) then
+        t_wall = t_wall + t % n(c2)
+        n_wall = n_wall + 1 
+      end if
+    end if
+  end do
+  
+  call Comm_Mod_Global_Sum_Real(t_wall)
+  call Comm_Mod_Global_Sum_Int(n_wall)
+ 
+  call Comm_Mod_Wait
+
+  t_wall = t_wall/n_wall
+
+  if(this_proc < 2) write(*,*) 'Twall = ', t_wall
+ 
   do c = 1, grid % n_cells
     if(heat_transfer) then
-      if(grid % wall_dist(c) < 1.0) then
-        flow % t_ref_f(c) = 5.0 + 4.0 * 1.0 
-!        flow % t_ref_f(c) = 21.9 + 21.5 * 1.0 
-      else
-        flow % t_ref_f(c) = 5.0 + 4.0 * grid % wall_dist(c) 
-!        flow % t_ref_f(c) = 21.9 + 21.5 * grid % wall_dist(c) 
-      end if        
+      flow % t_ref_f(c) = max(9.0, 5.0 + 4.0 * grid % wall_dist(c))
     end if
   end do ! through cells
 
