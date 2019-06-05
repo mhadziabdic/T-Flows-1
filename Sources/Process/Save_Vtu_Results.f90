7c04da923a44bc5579199a0bcff76c3102ae8e3f
mhadziabdic <muhamed.hadziabdic@gmail.com>
2019-06-05 09:09:23 +0200

    turbulent heat fluxes are added to vtu file.

--- Sources/Process/Save_Vtu_Results.f90
+++ Sources/Process/Save_Vtu_Results.f90
@@ -266,7 +266,10 @@
   if( (turbulence_model .eq. K_EPS_ZETA_F .and. heat_transfer) .or. &
       (turbulence_model .eq. HYBRID_LES_RANS .and. heat_transfer) ) then
     call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentQuantityT2", t2 % n(1))
-    call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentBuoyancyProduction", g_buoy(1))
+    if(buoyancy) call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentBuoyancyProduction", g_buoy(1))
+    call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentHeatFluxX", ut % n(1))
+    call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentHeatFluxY", vt % n(1))
+    call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentHeatFluxZ", wt % n(1))
 !    call Save_Vtu_Scalar(grid, IN_4, IN_5, "TurbulentT2Production", p_t2(1))
   end if
   ! Save vis and vis_t
