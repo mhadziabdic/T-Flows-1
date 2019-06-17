ba322a4628306308be2dbc824c69f0d4cfd986b9
mhadziabdic <muhamed.hadziabdic@gmail.com>
2019-06-17 10:49:39 +0200

    Minor changes

--- Sources/Process/Initialize_ref_and_initial_temperature.f90
+++ Sources/Process/Initialize_ref_and_initial_temperature.f90
@@ -50,31 +50,33 @@
     end do ! through cells
   end if
 
-  do s = 1, grid % n_faces
-    c1 = grid % faces_c(1,s)
-    c2 = grid % faces_c(2,s)
-
-    if(c2  < 0) then
-      if( Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. wallfl ) then
-        t_wall = t_wall + t % n(c2)
-        n_wall = n_wall + 1 
-      end if
-    end if
-  end do
+!  do s = 1, grid % n_faces
+!    c1 = grid % faces_c(1,s)
+!    c2 = grid % faces_c(2,s)
+!
+!    if(c2  < 0) then
+!      if( Grid_Mod_Bnd_Cond_Type(grid,c2) .eq. wallfl ) then
+!        t_wall = t_wall + t % n(c2)
+!        n_wall = n_wall + 1 
+!        t % q(c2) = 0.01
+!      end if
+!    end if
+!  end do
   
-  call Comm_Mod_Global_Sum_Real(t_wall)
-  call Comm_Mod_Global_Sum_Int(n_wall)
+!  call Comm_Mod_Global_Sum_Real(t_wall)
+!  call Comm_Mod_Global_Sum_Int(n_wall)
  
-  call Comm_Mod_Wait
+!  call Comm_Mod_Wait
+!
+!  t_wall = t_wall/n_wall
 
-  t_wall = t_wall/n_wall
-
-  if(this_proc < 2) write(*,*) 'Twall = ', t_wall
- 
-  do c = 1, grid % n_cells
-    if(heat_transfer) then
-      flow % t_ref_f(c) = max(t_wall, 5.0 + 4.0 * grid % wall_dist(c))
-    end if
-  end do ! through cells
+!  if(this_proc < 2) write(*,*) 'Twall = ', t_wall
+! 
+!  do c = 1, grid % n_cells
+!    if(heat_transfer) then
+!!      flow % t_ref_f(c) = max(t_wall, 5.0 + 4.0 * grid % wall_dist(c))
+!      flow % t_ref_f(c) = t_wall
+!    end if
+!  end do ! through cells
 
   end subroutine
